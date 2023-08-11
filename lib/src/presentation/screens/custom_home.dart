d508d5d60345b08065e8da2e826ced9620db0574
Olvine23 <olivinegeorge@gmail.com>
2023-08-11 15:06:47 +0300

    Added a search field

--- lib/src/presentation/screens/custom_home.dart
+++ lib/src/presentation/screens/custom_home.dart
@@ -34,13 +34,95 @@ class CustomHome extends StatelessWidget {
                     Row(
                       children: [
                         Icon(Icons.notifications),
-                        SizedBox(width: 8,),
+                        SizedBox(
+                          width: 8,
+                        ),
                         Icon(Icons.menu)
                       ],
                     )
                   ],
                 ),
-                
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 12.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search...',
+                      prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Current Balance",
+                    style: TextStyle(fontSize: 12, color: Color(0xff3D6ADA)),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "\$122229290",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w900, fontSize: 20),
+                    ),
+                    CircleAvatar(
+                      radius: 20,
+                        backgroundColor: Color(0xff3D6ADA),
+                        child: Icon(
+                          Icons.add,
+                          
+                          color: Colors.white,
+                        ))
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CircleAvatar(
+                      radius: 30,
+                        backgroundColor: Color(0xffD8E5FF),
+                        child: Icon(
+                          Icons.qr,
+
+                        color: Colors.black,
+                        )),
+                    CircleAvatar(
+                      radius: 30,
+                        backgroundColor: Color(0xffD8E5FF),
+                        child: Icon(
+                          Icons.add,
+                         color: Colors.black,
+                        )),
+                    CircleAvatar(
+                      radius: 30,
+                        backgroundColor: Color(0xffD8E5FF),
+                        child: Icon(
+                          Icons.add,
+                        
+                          color: Colors.black,
+                        )),
+                    CircleAvatar(
+                      radius: 30,
+                        backgroundColor: Color(0xffD8E5FF),
+                        child: Icon(
+                          Icons.add,
+                          color: Colors.black,
+                        ))
+                  ],
+                ),
+                SizedBox(
+                  height: 12,
+                ),
               ],
             ),
           ),
